a5ad239d509f842f3298714a6efc1d3a5a80840a
Ismail <77196863+iig01@users.noreply.github.com>
2022-10-28 11:29:36 +0500

    some bugs fixed

--- script.js
+++ script.js
@@ -2,28 +2,28 @@
 
 // Data
 const account1 = {
-  owner: 'Test1',
+  owner: 'Ismail Gaybullaev',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Test2',
+  owner: 'Test two',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Test3',
+  owner: 'Test three',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Test4',
+  owner: 'Test four',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
